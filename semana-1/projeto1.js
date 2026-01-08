c73dd2fe08b76c1366afe1402404578afdce9088
junior <118855761+juninhonew@users.noreply.github.com>
2026-01-08 04:09:48 -0300

    Projeto 1: variáveis e saída no terminal.

---
+++ semana-1/projeto1.js
@@ -0,0 +1,12 @@
+// Projeto 1 - JavaScript Básico
+// Objetivo: Praticar variáveis e console.log()
+
+
+let nome = "Rosildo";
+let idade = 23;
+let cidade = "João Pessoa";
+
+console.log("Meu nome é", nome, ", tenho", idade, "anos e moro em", cidade, "!");
+
+console.log('Meu nome é ' + nome + ', tenho ' + idade + ' anos e moro em ' + cidade + '!');
+
